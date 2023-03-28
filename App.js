abbcb8d8bd7aa6d25356d7cf3b668f8813e76cdd
yaronhai <72712518+yaronhai@users.noreply.github.com>
2023-03-28 23:23:00 +0300

    start with eslint and pretier

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExopStatusBar } from 'expo-status-bar';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={{ padding:16, backgroundColor:"green"}}>
+          <Text>search</Text>
+        </View>
+        <View style={{flex:1, padding:16, backgroundColor:"blue"}}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExopStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1,     
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight +5 : 0,
+  }
 });
